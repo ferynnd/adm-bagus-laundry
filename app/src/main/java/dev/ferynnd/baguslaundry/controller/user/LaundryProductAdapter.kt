978704e2959fc040e7160a69e8431550e49a71ed
hamdaniqhmqd <ahmad.hamdani1955@gmail.com>
2025-05-02 06:06:51 +0700

    mengalola item laundry tahap 1 (R)

---
+++ app/src/main/java/dev/ferynnd/baguslaundry/controller/user/LaundryProductAdapter.kt
@@ -0,0 +1,62 @@
+package dev.ferynnd.baguslaundry.controller.user
+
+import android.content.Intent
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import android.widget.Toast
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.ListAdapter
+import androidx.recyclerview.widget.RecyclerView
+import dev.ferynnd.baguslaundry.databinding.CardProductLaundryBinding
+import dev.ferynnd.baguslaundry.model.ProductLaundry
+
+class LaundryProductAdapter(
+    private val onItemClick: (ProductLaundry) -> Unit
+) : ListAdapter<ProductLaundry, LaundryProductAdapter.ProductLaundryViewHolder>(DiffCallback()) {
+
+    inner class ProductLaundryViewHolder(val binding: CardProductLaundryBinding) :
+        RecyclerView.ViewHolder(binding.root) {
+        val nama_laundry = binding.namaProductLaundry
+        val harga_waktu = binding.hargaWaktuProductLaundry
+        val deskripsi = binding.deskripsiProductLaundry
+
+        init {
+            itemView.setOnClickListener {
+                onItemClick(getItem(adapterPosition))
+            }
+        }
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ProductLaundryViewHolder {
+        val binding = CardProductLaundryBinding.inflate(
+            LayoutInflater.from(parent.context), parent, false
+        )
+        return ProductLaundryViewHolder(binding)
+    }
+
+    override fun onBindViewHolder(holder: ProductLaundryViewHolder, position: Int) {
+        val laundryItem = getItem(position)
+        var harga_laundry = laundryItem.price_laundry_item.toString()
+        var waktu_laundry = laundryItem.time_laundry_item.toString()
+
+        holder.nama_laundry.text = laundryItem.name_laundry_item.toString()
+        holder.harga_waktu.text = "$harga_laundry - $waktu_laundry"
+        holder.deskripsi.text = laundryItem.description_laundry_item.toString()
+
+        holder.itemView.setOnClickListener {
+            onItemClick(laundryItem)
+        }
+    }
+
+    class DiffCallback : DiffUtil.ItemCallback<ProductLaundry>() {
+        override fun areItemsTheSame(oldItem: ProductLaundry, newItem: ProductLaundry): Boolean {
+            return oldItem.id_laundry_item == newItem.id_laundry_item
+        }
+
+        override fun areContentsTheSame(oldItem: ProductLaundry, newItem: ProductLaundry): Boolean {
+            return oldItem == newItem
+        }
+    }
+
+
+}
--- app/src/main/java/dev/ferynnd/baguslaundry/data/api/LaundryProductApiService.kt
+++ app/src/main/java/dev/ferynnd/baguslaundry/data/api/LaundryProductApiService.kt
@@ -9,21 +9,33 @@ import retrofit2.http.DELETE
 import retrofit2.http.Path
 
 interface LaundryProductApiService {
+    @GET("api/{role}/laundry_items")
+    suspend fun getProductLaundry(
+        @Path("role") role: String,
+    ): ApiResponse<ProductLaundry>
 
-    @GET("api/laundry_items")
-    suspend fun getProductLaundry(): ApiResponse<ProductLaundry>
+    @POST("api/{role}/create_laundry_items")
+    suspend fun createProductLaundry(
+        @Path("role") role: String,
+        @Body productLaundry: ProductLaundry
+    ): DefaultRequest<ProductLaundry>
 
-    @POST("api/create_laundry_items")
-    suspend fun createProductLaundry( @Body productLaundry: ProductLaundry): DefaultRequest<ProductLaundry>
+    @GET("api/{role}/laundry_items/{id}")
+    suspend fun getProductLaundryById(
+        @Path("role") role: String,
+        @Path("id") id: Int
+    ): DefaultRequest<ProductLaundry>
 
-    @GET("api/laundry_items/{id}")
-    suspend fun getProductLaundryById(@Path("id") id: Int): DefaultRequest<ProductLaundry>
+    @DELETE("api/{role}/delete_laundry_items/{id}")
+    suspend fun deleteProductLaundry(
+        @Path("role") role: String,
+        @Path("id") id: Int
+    ): DefaultRequest<ProductLaundry>
 
-    @DELETE("api/delete_laundry_items/{id}")
-    suspend fun deleteProductLaundry(@Path("id") id: Int): DefaultRequest<ProductLaundry>
-
-    @PUT("api/edit_laundry_items/{id}")
-    suspend fun updateProductLaundry(@Path("id") id: Int, @Body productLaundry: ProductLaundry): DefaultRequest<ProductLaundry>
-
-
+    @PUT("api/{role}/edit_laundry_items/{id}")
+    suspend fun updateProductLaundry(
+        @Path("role") role: String,
+        @Path("id") id: Int,
+        @Body productLaundry: ProductLaundry
+    ): DefaultRequest<ProductLaundry>
 }
